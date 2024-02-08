c7800c643447e9b2e2c18e824d328ef0164b195f
IIODERII <oderrisi@gmail.com>
2024-02-08 15:48:29 +0100

    added components to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
+  <header>
+    <HeaderComponent />
+  </header>
   <div>
-
+    <router-view />
   </div>
+  <footer>
+    <FooterComponent />
+  </footer>
 </template>
 
 <script>
+import FooterComponent from './components/FooterComponent.vue';
+import HeaderComponent from './components/HeaderComponent.vue';
+import { store } from './data/store';
 export default {
   name: 'App',
   data() {
-    return {}
-  }
+    return {};
+  },
+  components: { HeaderComponent, FooterComponent }
 }
 </script>
 
