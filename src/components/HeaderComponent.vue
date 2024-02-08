fd48b0183ec8951c7e2771be1315cd6a7bc3a88a
davidelunetta <davidelunetta.27@gmail.com>
2024-02-08 18:33:23 +0100

    header structure updated

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -1,6 +1,26 @@
 <template>
     <div>
-
+        <nav class="navbar navbar-expand-lg">
+            <div class="container-fluid">
+                <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarNavAltMarkup">
+                    <div class="navbar-nav">
+                        <ul class="d-flex list-unstyled">
+                            <li class="nav-item "><router-link class="nav-link text-white fs-5" to="/">Prova 1</router-link></li>
+                            <li class="nav-item "><router-link class="nav-link text-white fs-5" to="/">Prova 2</router-link></li>
+                            <li class="nav-item "><router-link class="nav-link text-white fs-5" to="/">Prova 3</router-link></li>
+                            <li class="nav-item "><router-link class="nav-link text-white fs-5" to="/">Prova 4</router-link></li>
+                        </ul>
+                    </div>
+                    <div class="d-flex">
+                        <ul class="d-flex list-unstyled">
+                            <li class="nav-item text-white me-3"><i class="fa-solid fa-magnifying-glass"></i></li>
+                            <li class="nav-item text-white me-3"><i class="fa-solid fa-cart-shopping"></i></li>
+                            <li class="nav-item text-white me-3"><i class="fa-solid fa-user"></i></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </nav>
     </div>
 </template>
 
@@ -8,6 +28,12 @@
 export default {
     name: 'HeaderComponent',
 }
+
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use ".././assets/styles/partials/variables" as *;
+nav{
+    background-color: $bg-primary;
+}
+</style>
