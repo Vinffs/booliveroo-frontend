64b0f2ca4bd32c5baaf6139a3a524cb112bb4bb4
IIODERII <oderrisi@gmail.com>
2024-02-08 18:52:08 +0100

    completed RestaurantsHome component

--- src/components/partials/SponsoredRestaurants.vue
+++ src/components/partials/SponsoredRestaurants.vue
@@ -1,15 +1,22 @@
 <template>
   <div>
     <RestaurantsTitle :title="'Ristoranti Sponsorizzati'" />
+    <div class="row">
+      <div class="col-12 col-md-6 px-3" v-for="i in 2">
+        <RestaurantCard :restaurant="null" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import RestaurantsTitle from "../partials/RestaurantsTitle.vue";
+import RestaurantCard from "../partials/RestaurantCard.vue";
 export default {
   name: "SponsoredRestaurants",
   components: {
     RestaurantsTitle,
+    RestaurantCard,
   },
 };
 </script>
