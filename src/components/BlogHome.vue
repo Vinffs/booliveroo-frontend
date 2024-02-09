c230e871a50ab1ba245ded2a0dc98dc509cc3440
davidelunetta <davidelunetta.27@gmail.com>
2024-02-09 10:36:50 +0100

    card style modified

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -2,6 +2,43 @@
     <div class="blog">
         <h2 class="text-center text-white fw-semibold m-0 pt-5">Read Our Blog</h2>
         <div class="pt-5 pb-5 container d-flex justify-content-between align-items-center">
+            <div class="card rounded my-card">
+                <img class="card-img-top rounded mx-auto d-block" src="../../public/images/piatto-1.webp" alt="piatto-1">
+                <div class="card-body">
+                    <h5 class="card-title fw-semibold text-white">Questa è una prova</h5>
+                    <p class="card-text text-white">Questa è un'altra prova</p>
+                    <span class="user d-flex">
+                        <img class="me-1" src="../../public/images/among_us.jpg" alt="user-profile">
+                        <p class="text-white">salam piccant</p>
+                    </span>
+                </div>
+            </div>
+            <div class="card rounded my-card mx-2">
+                <img class="card-img-top rounded mx-auto d-block" src="../../public/images/piatto-2.jpg" alt="piatto-2">
+                <div class="card-body">
+                    <h5 class="card-title fw-semibold text-white">Questa è una prova</h5>
+                    <p class="card-text text-white">Questa è un'altra prova</p>
+                    <span class="user d-flex">
+                        <img class="me-1" src="../../public/images/among_us.jpg" alt="user-profile">
+                        <p class="text-white">salam piccant</p>
+                    </span>
+                </div>
+            </div>
+            <div class="card rounded my-card">
+                <img class="card-img-top rounded mx-auto d-block" src="../../public/images/piatto-3.webp" alt="piatto-3">
+                <div class="card-body">
+                    <h5 class="card-title fw-semibold text-white">Questa è una prova</h5>
+                    <p class="card-text text-white">Questa è un'altra prova</p>
+                    <span class="user d-flex">
+                        <img class="me-1" src="../../public/images/among_us.jpg" alt="user-profile">
+                        <p class="text-white">salam piccant</p>
+                    </span>
+                </div>
+            </div>
+        </div>
+
+
+        <!-- <div class="pt-5 pb-5 container d-flex justify-content-between align-items-center">
             <div class="rounded">
                 <img class="rounded mx-auto d-block" src="../../public/images/piatto-1.webp" alt="piatto-1">
                 <h5 class="fw-semibold text-white">Questa è una prova</h5>
@@ -17,7 +54,7 @@
                 <h5 class="fw-semibold text-white">Questa è una prova</h5>
                 <span>questa è un'altra prova</span>
             </div>
-        </div>
+        </div> -->
     </div>
 </template>
 
@@ -33,7 +70,17 @@ export default {
     background-color: $bg-secondary;
 }
 img{
-    width: 180px;
-    height: 150px;
+    width: 100%;
+    height: 300px;
+}
+.my-card{
+    background-color: $bg-primary;
+}
+.user{
+    img{
+        width: 30px;
+        height: 30px;
+        border-radius: 50%;
+    }
 }
 </style>
