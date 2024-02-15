2114d392ecaa1a3fd1d08c2700fc53ce20c32000
MaxPredathor <max.gilli98@gmail.com>
2024-02-15 12:34:27 +0100

    RestaruantMenu component completed

--- src/components/RestaurantBadge.vue
+++ src/components/RestaurantBadge.vue
@@ -33,7 +33,6 @@ export default {
 
 .wrapper {
     width: 100%;
-    height: 100%;
     background-color: $bg-secondary !important;
 
     .badge-image {
--- src/components/RestaurantMenu.vue
+++ src/components/RestaurantMenu.vue
@@ -1,12 +1,28 @@
 <template>
-    <div class="wrapper">
-
+    <div class="wrapper rounded-3 p-3">
+        <div v-for="item in info.dishes" class="dish p-3 d-flex align-items-center justify-content-evenly">
+            <div class="text-white d-flex flex-column align-items-center gap-3">
+                <h4>{{ item.name }}</h4>
+                <p>{{ item.ingredients }}</p>
+                <p>€ {{ item.price }}</p>
+            </div>
+            <img class="w-50" :src="store.imagePath + item.image" alt="">
+        </div>
     </div>
 </template>
 
 <script>
+import { store } from "../data/store";
 export default {
-    name: "RestaurantMenu"
+    name: "RestaurantMenu",
+    props: {
+        info: Object,
+    },
+    data() {
+        return {
+            store,
+        }
+    }
 }
 </script>
 
@@ -15,7 +31,15 @@ export default {
 
 .wrapper {
     width: 100%;
-    height: 100%;
     background-color: $bg-secondary !important;
+
+    .dish {
+        width: 100%;
+
+        h4,
+        p {
+            margin: 0;
+        }
+    }
 }
 </style>
