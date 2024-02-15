26d6123046a683841f0cfcc47c9830752f419351
MaxPredathor <max.gilli98@gmail.com>
2024-02-15 09:35:25 +0100

    created restaurant show views

---
+++ src/pages/RestaurantShow.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import { store } from '../data/store';
+export default {
+    name: 'RestaurantShow',
+    data() {
+        return {
+            store,
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+</style>
