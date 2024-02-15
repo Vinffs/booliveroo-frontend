aa9e5d6e318b8e217fa8caed005568a4b512f7cf
MaxPredathor <max.gilli98@gmail.com>
2024-02-15 11:26:43 +0100

    RestourantBadge component completed

--- src/components/RestaurantBadge.vue
+++ src/components/RestaurantBadge.vue
@@ -1,9 +1,15 @@
 <template>
-    <div class="wrapper p-3 rounded-3 text-white">
+    <div class="wrapper p-3 rounded-3 text-white d-flex">
         <div class="badge-image">
             <img :src="store.imagePath + info.image" :alt="info.name">
         </div>
-        <div class="badge-text"></div>
+        <div class="badge-text p-4 d-flex align-items-center justify-content-center flex-column">
+            <h3 class="fw-bold pt-2">{{ info.name }}</h3>
+            <div class="d-flex gap-3 pt-2">
+                <p class="fs-6 dash" v-for="category in info.categories">{{ category.name }}</p>
+            </div>
+            <p class="fs-6">{{ info.address }}</p>
+        </div>
     </div>
 </template>
 
@@ -28,10 +34,38 @@ export default {
 .wrapper {
     width: 100%;
     height: 100%;
+    background-color: $bg-secondary !important;
 
     .badge-image {
-        width: 20%;
+        width: 25%;
         height: 100%;
+
+        img {
+            width: 100%;
+            object-fit: contain;
+            object-position: center center;
+            border-radius: 0.8em;
+        }
+    }
+
+    .badge-text {
+        width: 75%;
+        height: 100%;
+
+        .dash {
+
+
+            &::after {
+                content: " • ";
+                padding-left: 10px;
+            }
+
+            &:last-child {
+                &::after {
+                    content: "";
+                }
+            }
+        }
     }
 }
 </style>
