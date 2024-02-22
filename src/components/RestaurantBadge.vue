ed94a39a4f4af166ea4a73f27c9f22922b56bced
MaxPredathor <max.gilli98@gmail.com>
2024-02-22 14:56:38 +0100

    added darkMode to restaurant page

--- src/components/RestaurantBadge.vue
+++ src/components/RestaurantBadge.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="wrapper p-3 rounded-3 d-flex">
+  <div class="wrapper p-3 rounded-3 d-flex" :class="(store.darkMode ? 'dark-secondary' : 'light-secondary')">
     <div class="badge-image">
       <img :src="store.imagePath + info.image" :alt="info.name" />
     </div>
-    <div
-      class="badge-text p-4 d-flex align-items-center justify-content-center flex-column"
-    >
+    <div class="badge-text p-4 d-flex align-items-center justify-content-center flex-column"
+      :class="(store.darkMode ? 'text-light' : 'text-dark')">
       <h3 class="fw-bold pt-2">{{ info.name }}</h3>
       <div class="d-flex gap-3 pt-2">
         <p class="fs-6 dash" v-for="category in info.categories">
@@ -37,7 +36,7 @@ export default {
 
 .wrapper {
   width: 100%;
-  background-color: $bg-secondary !important;
+  // background-color: $bg-secondary !important;
 
   .badge-image {
     width: 25%;
--- src/components/RestaurantMenu.vue
+++ src/components/RestaurantMenu.vue
@@ -1,9 +1,6 @@
 <template>
-  <div class="wrapper rounded-3">
-    <div
-      v-for="item in info.dishes"
-      class="dish p-4 d-flex align-items-center justify-content-evenly"
-    >
+  <div class="wrapper rounded-3" :class="(store.darkMode ? 'text-light dark-secondary' : 'text-dark light-secondary')">
+    <div v-for="item in info.dishes" class="dish p-4 d-flex align-items-center justify-content-evenly">
       <div class="d-flex flex-column align-items-center gap-3">
         <h4>{{ item.name }}</h4>
         <p>{{ item.ingredients }}</p>
@@ -13,11 +10,7 @@
         </div>
       </div>
       <div class="w-50 image-container rounded-2">
-        <img
-          class="dish-image"
-          :src="store.imagePath + item.image"
-          :alt="item.name"
-        />
+        <img class="dish-image" :src="store.imagePath + item.image" :alt="item.name" />
       </div>
     </div>
   </div>
@@ -58,11 +51,12 @@ export default {
     border-bottom: 2px solid $bg-primary;
     width: 100%;
     max-height: 270px;
-    background-color: $bg-secondary !important;
+    // background-color: $bg-secondary !important;
 
     .image-container {
       overflow: hidden;
       max-height: 238px;
+
       .dish-image {
         width: 100%;
         object-fit: contain;
