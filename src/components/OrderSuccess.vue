f4640055dde1dbbef6ac468e187495dbfa581119
IIODERII <oderrisi@gmail.com>
2024-02-23 10:29:59 +0100

    styled order complete

--- src/components/OrderSuccess.vue
+++ src/components/OrderSuccess.vue
@@ -1,13 +1,20 @@
 <template>
-    <div>
-        <h1>Ordine effettuato con successo!</h1>
-    </div>
+  <div class="my-wrapper">
+    <h1>Ordine effettuato con successo!</h1>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "OrderSuccess",
-}
+  name: "OrderSuccess",
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+.my-wrapper {
+  padding: 50px;
+  background-color: $bg-secondary;
+  border-radius: 3em;
+}
+</style>
--- src/pages/OrderCompleted.vue
+++ src/pages/OrderCompleted.vue
@@ -1,8 +1,8 @@
 <template>
-    <div>
-        <OrderSuccess v-if="$route.params.success" />
-        <OrderFailed v-else />
-    </div>
+  <div class="wrapper-tot d-flex justify-content-center align-items-center">
+    <OrderSuccess v-if="$route.params.success" />
+    <OrderFailed v-else />
+  </div>
 </template>
 
 <script>
@@ -10,18 +10,24 @@ import { store } from "../data/store";
 import OrderSuccess from "../components/OrderSuccess.vue";
 import OrderFailed from "../components/OrderFailed.vue";
 export default {
-    name: "OrderCompleted",
-    data() {
-        return {
-            store,
-        }
-    },
-    components: {
-        OrderFailed,
-        OrderSuccess
-
-    }
-}
+  name: "OrderCompleted",
+  data() {
+    return {
+      store,
+    };
+  },
+  components: {
+    OrderFailed,
+    OrderSuccess,
+  },
+};
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use "../assets/styles/partials/variables" as *;
+.wrapper-tot {
+  position: relative;
+  z-index: 10;
+  min-height: 70vh;
+}
+</style>
