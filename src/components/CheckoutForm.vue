0eb7146657fb7f972279f79a96480188711a7e1e
davidelunetta <davidelunetta.27@gmail.com>
2024-02-23 15:51:42 +0100

    order detail updated

--- src/components/CheckoutForm.vue
+++ src/components/CheckoutForm.vue
@@ -314,7 +314,11 @@ export default {
           .then((res) => {
             console.log(res.data);
             store.checkout = res.data;
-            this.$router.push("/order-status");
+            this.$router.push({
+            name: 'order-status',
+            query: { data: JSON.stringify(res.data) }
+          });
+            
           })
           .catch((err) => {
             this.$router.push("/order-status");
