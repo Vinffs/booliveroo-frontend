67353585030008af8df7750a7b684d6d381b581a
MaxPredathor <max.gilli98@gmail.com>
2024-02-25 14:42:40 +0100

    fixed dark mode on blog component

--- src/components/BlogHome.vue
+++ src/components/BlogHome.vue
@@ -1,121 +1,70 @@
 <template>
   <div class="blog">
-    <h2
-      class="text-center fw-semibold m-0 pt-5"
-      :class="store.darkMode ? 'text-light' : 'text-dark'"
-    >
+    <h2 class="text-center fw-semibold m-0 pt-5" :class="store.darkMode ? 'text-light' : 'text-dark'">
       Ricette consigliate
     </h2>
     <div class="pt-5 pb-5 container">
       <div class="row justify-content-center align-items-center">
-        <div
-          class="card rounded my-card col-12 col-md-6 col-lg-4"
-          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'"
-        >
-          <a
-            class="card-content text-decoration-none"
-            href="https://ricette.giallozafferano.it/Spaghetti-alla-Carbonara.html"
-            target="”_blank”"
-          >
-            <div
-              class="card-image spaghetti"
-              :class="store.darkMode ? 'image-dark' : 'image-light'"
-            >
+        <div class="card rounded my-card col-12 col-md-6 col-lg-4"
+          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'">
+          <a class="card-content text-decoration-none"
+            href="https://ricette.giallozafferano.it/Spaghetti-alla-Carbonara.html" target="”_blank”">
+            <div class="card-image spaghetti" :class="store.darkMode ? 'image-dark' : 'image-light'">
               <!-- <img class="card-img-top rounded mx-auto d-block" src="/images/piatto-1.webp" alt="piatto-1" /> -->
             </div>
-            <div
-              class="card-body"
-              :class="
-                (store.darkMode ? 'dark' : 'light',
-                store.darkMode ? 'text-light' : 'text-dark')
-              "
-            >
-              <h5 class="card-title fw-semibold">Spaghetti alla Carbonara</h5>
+            <div class="card-body" :class="(store.darkMode ? 'dark' : 'light')
+              ">
+              <h5 class="card-title fw-semibold" :class="(store.darkMode ? 'text-light' : 'text-dark')">Spaghetti alla
+                Carbonara</h5>
               <p class="card-text">
                 <span class="fw-semibold">Da: </span>Giallo Zafferano
               </p>
               <span class="user d-flex">
-                <img
-                  class="me-1"
-                  src="/images/blog/mirko.png"
-                  alt="user-profile"
-                />
+                <img class="me-1" src="/images/blog/mirko.png" alt="user-profile" />
                 <p class="">Mirko Marasco</p>
               </span>
             </div>
           </a>
         </div>
-        <div
-          class="card rounded my-card col-12 col-md-6 col-lg-4"
-          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'"
-        >
-          <a
-            class="card-content text-decoration-none"
+        <div class="card rounded my-card col-12 col-md-6 col-lg-4"
+          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'">
+          <a class="card-content text-decoration-none"
             href="https://ricette.giallozafferano.it/Risotto-ai-mirtilli-di-bosco-e-erba-cipollina.html"
-            target="”_blank”"
-          >
-            <div
-              class="card-image risotto"
-              :class="store.darkMode ? 'image-dark' : 'image-light'"
-            >
+            target="”_blank”">
+            <div class="card-image risotto" :class="store.darkMode ? 'image-dark' : 'image-light'">
               <!-- <img class="card-img-top rounded mx-auto d-block" src="/images/piatto-2.jpg" alt="piatto-2" /> -->
             </div>
-            <div
-              class="card-body"
-              :class="
-                (store.darkMode ? 'dark' : 'light',
-                store.darkMode ? 'text-light' : 'text-dark')
-              "
-            >
-              <h5 class="card-title fw-semibold">
+            <div class="card-body" :class="(store.darkMode ? 'dark' : 'light')
+              ">
+              <h5 class="card-title fw-semibold" :class="(store.darkMode ? 'text-light' : 'text-dark')">
                 Risotto ai mirtilli di bosco
               </h5>
               <p class="card-text">
                 <span class="fw-semibold">Da: </span>Giallo Zafferano
               </p>
               <span class="user d-flex">
-                <img
-                  class="me-1"
-                  src="/images/blog/oder.png"
-                  alt="user-profile"
-                />
+                <img class="me-1" src="/images/blog/oder.png" alt="user-profile" />
                 <p class="">Oder Risi</p>
               </span>
             </div>
           </a>
         </div>
-        <div
-          class="card rounded my-card col-12 col-md-6 col-lg-4"
-          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'"
-        >
-          <a
-            class="card-content text-decoration-none"
-            href="https://ricette.giallozafferano.it/Tortellini-in-brodo.html"
-            target="”_blank”"
-          >
-            <div
-              class="card-image tortellini"
-              :class="store.darkMode ? 'image-dark' : 'image-light'"
-            >
+        <div class="card rounded my-card col-12 col-md-6 col-lg-4"
+          :class="store.darkMode ? 'dark-secondary' : 'light-secondary'">
+          <a class="card-content text-decoration-none" href="https://ricette.giallozafferano.it/Tortellini-in-brodo.html"
+            target="”_blank”">
+            <div class="card-image tortellini">
               <!-- <img class="card-img-top rounded mx-auto d-block" src="/images/piatto-3.webp" alt="piatto-3" /> -->
             </div>
-            <div
-              class="card-body"
-              :class="
-                (store.darkMode ? 'dark' : 'light',
-                store.darkMode ? 'text-light' : 'text-dark')
-              "
-            >
-              <h5 class="card-title fw-semibold">Tortellini in Brodo</h5>
+            <div class="card-body" :class="(store.darkMode ? 'dark' : 'light')
+              ">
+              <h5 class="card-title fw-semibold" :class="(store.darkMode ? 'text-light' : 'text-dark')">Tortellini in
+                Brodo</h5>
               <p class="card-text">
                 <span class="fw-semibold">Da:</span> Giallo Zafferano
               </p>
               <span class="user d-flex">
-                <img
-                  class="me-1"
-                  src="/images/blog/max.png"
-                  alt="user-profile"
-                />
+                <img class="me-1" src="/images/blog/max.png" alt="user-profile" />
                 <p class="">Massimiliano Gilli</p>
               </span>
             </div>
